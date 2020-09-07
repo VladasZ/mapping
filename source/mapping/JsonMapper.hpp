6f4b5b344e2de52b74abbdf4ad8468207cb56585
Vladas Zakrevskis <146100@gmail.com>
2020-09-07 22:34:18 +0300

    lin fix

--- source/mapping/JsonMapper.hpp
+++ source/mapping/JsonMapper.hpp
@@ -50,7 +50,7 @@ namespace mapping {
                 return parse<Class>(json);
             }
             catch (...) {
-                return what();
+                return {};// what();
             }
         }
 
