a395db95523fc3613d3ed474634c075d2ade7756
Vladas Zakrevskis <146100@gmail.com>
2021-03-14 13:31:44 +0300

    testing

--- source/mapping_test/main.cpp
+++ source/mapping_test/main.cpp
@@ -8,17 +8,29 @@ using namespace cu;
 using namespace mapping;
 
 
-class Obj {
+struct Nested {
 
 };
+
+struct Obj {
+    Nested a;
+    Nested get_value_a() { return a; }
+    Nested& get_ref_a() { return a; }
+    const Nested& get_const_ref_a() { return a; }
+};
+
+using Get = decltype(&Obj::get_value_a);
+using GetRef = decltype(&Obj::get_ref_a);
+using GetConstRef = decltype(&Obj::get_const_ref_a);
+
+using GetInfo = cu::pointer_to_member_info<Get>;
+using GetRefInfo = cu::pointer_to_member_info<GetRef>;
+using GetConstRefInfo = cu::pointer_to_member_info<GetConstRef>;
+
 
 template <class T>
 void test_template(const T& obj) {
     using ObjT = decltype(obj);
-
-    Logvar(class_name(obj));
-    Logvar(class_name<T>());
-    Logvar(class_name<ObjT>());
     Logvar(std::is_pointer_v<T>);
     Logvar(std::is_reference_v<T>);
     Logvar(std::is_pointer_v<ObjT>);
@@ -29,9 +41,27 @@ void test_template(const T& obj) {
 int main() {
 
 
-    test_template(new Obj());
+
+    Obj obj;
+
+//    test_template(new Obj());
+//    Separator;
+//    test_template(Obj());
+
     Separator;
-    test_template(Obj());
+
+    Logvar(GetInfo::to_string());
+    Logvar(GetRefInfo::to_string());
+    Logvar(GetConstRefInfo::to_string());
+
+//    REVEAL_TEMPLATE(Spok);
+
+    Separator;
+
+
+    Logvar(CU_CLASS_NAME(
+            decltype(std::declval<Obj>().get_const_ref_a())
+            ));
 
 //    test();
 
